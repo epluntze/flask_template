71bda3d137fcbe700f3181552cdd4c0fcb6faced
epluntze <eric@sandboxx.us>
2022-02-03 12:46:08 -0500

    fix create app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,10 +36,10 @@ def create_app(test_config=None):
     db.init_app(app)
 
     # apply the blueprints to the app
-    from flaskr import auth, blog
+    from flaskr import developers, tasks
 
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
+    app.register_blueprint(developers.bp)
+    app.register_blueprint(tasks.bp)
 
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with
